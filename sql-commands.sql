a88e4325559451470426ef11d7a1b0b5bf3d9877
bhanukandregula <bhanugates@gmail.com>
2022-05-10 10:22:22 -0400

    Updated SQL commands

--- sql-commands.sql
+++ sql-commands.sql
@@ -7,5 +7,77 @@ CREATE TABLE employees (
     emp_client VARCHAR(20)
 );
 
+-- This will create a table, having conditions where name can't be NUL, student_id is the primary key 
+-- and major must be UNIQUE for each record
+CREATE TABLE student (
+    student_id INT,
+    student_name VARCHAR(30) NOT NULL, 
+    major VARCHAR(30) UNIQUE,
+    PRIMARY KEY(student_id)
+);
+
+-- Same as above conditions met, but where major will be 'UNDECIDED' 
+-- If we don;t pass ther major value in INSERT command, it will be UNDECIDED by default.
+CREATE TABLE student (
+    student_id INT,
+    student_name VARCHAR(30) NOT NULL, 
+    major VARCHAR(30) DEFAULT 'UNDECIDED',
+    PRIMARY KEY(student_id)
+);
+
+-- This has also met few above conditions, also AUTO_INCREMENT will allow us to 
+-- keep the student_id increate automatically for each recored we inserted.
+
+-- so we can stop student_id value while we insert records if requried.
+CREATE TABLE student (
+    student_id INT AUTO_INCREMENT,
+    student_name VARCHAR(30) NOT NULL, 
+    major VARCHAR(30) DEFAULT 'UNDECIDED',
+    PRIMARY KEY(student_id)
+)
+
 -- This will fetch all the records in the database
 SELECT * FROM employees;
+
+-- This will give table details
+DESCRIBE employees;
+
+-- This will delete table completely from database
+DROP TABLE employees;
+
+-- This will add a new column to existing table
+ALTER TABLE employees ADD emp_salary VARCHAR(10);
+
+-- This will delete the column in exisitng table
+ALTER TABLE employees DROP COLUMN emp_salary;
+
+-- Insert records into table
+-- Order of values must be as same as we have in create table command.
+INSERT INTO employees VALUES(001, 'Bhanu', 'bkandreg@gmail.com','Full Stack','Ford');
+
+-- This will only insert specified columns in a new record. other columns will remain with null values
+INSERT INTO employees(emp_id, emp_name, emp_client) VALUES(002, 'Alex', 'Walmart');
+
+-- This will show records where conditions only met
+SELECT * FROM employees where emp_client='Ford';
+
+-- This will update specific record where conditions met.
+UPDATE employees SET emp_client='Google' where emp_id=1;
+
+-- This udpates records where both conditions met
+UDPATE employees SET emp_client='IBM' WHERE emp_client='Ford Credit' OR emp_client='Aetna';
+
+-- Update/ SET multiple columns in a single record
+UPDATE employees SET emp_name='Cash', emp_tech='Blockchain' where emp_id=1;
+
+-- Delete all the rows in a Table
+DELETE FROM employees;
+
+-- Delete specific record from the Table, where conditions met
+DELETE FROM employees where emp_id=1;
+
+-- Select only few columns in a table
+SELECT emp_name, emp_client FROM employees;
+
+-- This will display records with ORDER by condition, ascending order is the defualt one.
+SELECT emp_name, emp_client FROM employees ORDER BY emp_name ASC;
